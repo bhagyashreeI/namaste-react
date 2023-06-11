17b948b65815313c80d59af159734ebb063f48eb
Bhagyashree Rahile <bhagyashreerahile@gmail.com>
2023-06-11 13:10:26 +0530

    all from episode 1

---
+++ App.js
@@ -0,0 +1,7 @@
+const heading = React.createElement("h1",{id:"heading",xyz:"newattr"},"Hello World from React");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+const headerText = React.createElement('h1',{},'This is header');
+const header = ReactDOM.createRoot(document.getElementById('header'));
+header.render(headerText);
