4b1552287bc101127cb190879a7d5a1cc1ae2241
Bhagyashree Rahile <bhagyashreerahile@gmail.com>
2023-06-24 14:59:17 +0530

    just commit

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const heading = React.createElement("h1",{id:"heading",xyz:"newattr"},"Hello World from React");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
 
-const headerText = React.createElement('h1',{},'This is header');
-const header = ReactDOM.createRoot(document.getElementById('header'));
-header.render(headerText);
+const TitleComponent = () => <h1>This id Title Component..</h1>
+
+const para = (
+    <p>test para</p>
+)
+const para2 = (
+    <p>{para}test para2<TitleComponent/></p>
+    
+)
+
+const count =5;
+const HeadingComponent = () => {
+    return <div className='container' style={{
+        backgroundColor:"pink",
+        display:"flex"
+    }}>
+        <TitleComponent/>
+        {count }
+        {console.log("hi")}
+    <h2 className='header'>This is Functional Component.</h2>
+        {para2}
+    </div>
+}
+const header = ReactDOM.createRoot(document.getElementById('root'));
+header.render(<HeadingComponent/>);
